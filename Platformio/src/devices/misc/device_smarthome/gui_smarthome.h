8977ec0b5f88d8301e8ce0094fa6a28e577d2caa
joe288 <joe288@live.de>
2025-11-15 09:18:08 +0100

    ?

--- Platformio/src/devices/misc/device_smarthome/gui_smarthome.h
+++ Platformio/src/devices/misc/device_smarthome/gui_smarthome.h
@@ -2,6 +2,6 @@
 
 #include <lvgl.h>
 
-const char * const tabName_smarthome = "Smart Home";
+const char * const tabName_smarthome = "Smart Home Licht";
 extern uint16_t GUI_SMARTHOME_ACTIVATE;
 void register_gui_smarthome(void);
